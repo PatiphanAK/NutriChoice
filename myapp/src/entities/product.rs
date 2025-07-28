6ecb0e1b92e39d7b62b56020046f41c41897acbc
PatiphanAK <pakkahadsri@gmail.com>
2025-07-29 01:19:38 +0700

    update constructor

--- myapp/src/entities/product.rs
+++ myapp/src/entities/product.rs
@@ -1,10 +1,19 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize)]
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
 pub struct Product {
     pub id: u32,
     pub name: String,
     pub price: f64,
+    pub calories: f64,
+    pub suger: f64,
+    pub sodium: f64,
+    pub protein: f64,
+    pub cholesterol: f64,
+    pub total_fat: f64,
+    pub saturated_fat: f64,
+    pub total_carbohydrate: f64,
+    pub dietary_fiber: f64,
 }
 
 #[derive(Debug, Deserialize)]
@@ -21,6 +30,11 @@ pub struct UpdateProduct {
 
 impl Product {
     pub fn new(id: u32, name: String, price: f64) -> Self {
-        Product { id, name, price }
+        Product {
+            id,
+            name,
+            price,
+            ..Default::default()
+        }
     }
 }
